e8247a8a5f3aceac5b7511b800f07aff283ac07d
Tan SRV <nguyenvantan7564@gmail.com>
2024-12-24 22:47:11 -0800

    12/25/2024

--- Settings/settings.py
+++ Settings/settings.py
@@ -4,105 +4,123 @@ import codecs
 from PyQt6.QtWidgets import QTextBrowser
 from PyQt6 import QtWidgets
 
-from NPC_Auto.lib_common.common_lib import *
+from NPC_Auto.lib_common.common_lib import _object_click, _open_app, _object_is_exist
 from PyQt6 import uic
 
-# Khởi tạo giao diện hiển thị kết quả
+# Initialize UI
 app = QtWidgets.QApplication(sys.argv)
 window = uic.loadUi("setting_result_display.ui")
 txt_result = window.findChild(QTextBrowser, 'txtResult')
 
-# Mảng chứa danh sách kết quả pass fail
+# The list contain information show on UI
 result_of_testcase = []
 
+# Initialize the log file
 with codecs.open("setting_logs.txt", "w", "utf-8") as file:
-    file.write('')
-# Hàm ghi thông tin pass fail vào log
+    file.write('LOG INFORMATION\n')
+
+# Function to write logs
 def write_log_setting(testcase_name,pass_list, fail_list):
+    # Open log file and write
     with codecs.open("setting_logs.txt", "a", "utf-8") as file:
-        file.write(f"\n{testcase_name}\n")
-        file.write("Danh sach pass.\n\t")
-        for pass_ob in pass_list:
-            file.write(f"{pass_ob}, ")
+        file.write(f"*{testcase_name.upper()}\n")
+        file.write("-List of pass: ")
+        file.write(", ".join(pass_list))
+        file.write("\n")
         if len(fail_list) > 0:
-            result_of_testcase.append(f"{testcase_name} Fail")
-            file.write("\nDanh sach fail.\n\t")
-            for fail_ob in fail_list:
-               file.write(f"{fail_ob}, ")
+            result_of_testcase.append(f"-{testcase_name}: Fail")
+            file.write("-List of fail: ")
+            file.write(", ".join(fail_list))
+            # for fail_ob in fail_list:
+            #    file.write(f"{fail_ob}, ")
+            file.write("\n")
         else:
-            result_of_testcase.append(f"{testcase_name} Pass")
+            result_of_testcase.append(f"-{testcase_name}: Pass")
 
+# Function to check if objects exist
+def _setting(testcase_name, app, dic_object_list):
+    # Open app settings
+    target_window = _open_app(f"{app}")
 
-# test case setting 3
-def _setting_3():
-    # Mở app settings
-    target_window = open_app('Settings')
-    # Danh sách các object
-    dic_object_list = {
-        'title': 'Find a setting, Home, System, Bluetooth & devices, Network & internet, Personalization, Apps, Accounts, '
-                 'Time & languagee, Gaming, Accessibility, Privacy & security, Windows Update',
-        'auto_id:': '',
-        'control_type': 'Text, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem'
-    }
-    # chuyển tilte and control type thành mảng
+    # Change title, control type, auto_id, object_handle to List
     titles = dic_object_list['title'].split(', ')
     control_types = dic_object_list['control_type'].split(', ')
-
-    # Mảng chứa thông tin các object pass fail
+    auto_ids = dic_object_list['auto_id'].split(', ')
+    object_handles = dic_object_list['object_handle'].split(', ')
+    # The List contains the pass fail objects
     pass_list = []
     fail_list = []
 
-    # Kiểm tra xem độ dài của các danh sách có khớp nhau không
-    if len(titles) == len(control_types):
-        for title, control_type in zip(titles, control_types):
-            is_exist = object_is_exist(target_window, title, '', control_type)
+    # Check that the lengths of the lists match
+    if len(titles) == len(control_types) == len(auto_ids) == len(object_handles):
+        is_exist = True
+        for title, auto_id, control_type, object_handle in zip(titles, auto_ids, control_types, object_handles):
+            if object_handle == 'click':
+                is_exist = _object_click(target_window, title, auto_id, control_type)
+            elif object_handle == 'view':
+                is_exist = _object_is_exist(target_window, title, auto_id, control_type)
             if is_exist:
                 pass_list.append(title)
             else:
                 fail_list.append(title)
     else:
-        print("Title và control type không đồng nhất")
+        print("Dic object list không đồng nhất")
 
-    # Đóng app
+    # Close app
     target_window.close()
-    write_log_setting('Setting 3',pass_list,fail_list)
+    write_log_setting(testcase_name, pass_list, fail_list)
 
-# Hàm test case setting 4
-def _setting_4():
-    # Mở app settings
-    target_window = open_app('Settings')
-
-    # Danh sách các object
-    dic_object = {
-        'title': 'Home, Recommended settings, Cloud storage, Bluetooth devices, Personalize your device, Try Microsoft 365',
-        'auto_id': ['', 'TitleContent', '', 'TitleContent', 'TitleContent', ''],
-        'control_type': 'ListItem, Text, Text, Text, Text, Text'
+# Function test case setting 3
+def _setting_3():
+    # dictionaries
+    dic_of_objects = {
+        'title': 'Find a setting, Home, System, Bluetooth & devices, Network & internet, Personalization, Apps, Accounts, '
+                 'Time & language, Gaming, Accessibility, Privacy & security, Windows Update',
+        'auto_id': ', , , , , , , , , , , , ',
+        'control_type': 'Text, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem, ListItem',
+        'object_handle': 'view, view, view, view, view, view, view, view, view, view, view, view, view'
     }
-    titles = dic_object['title'].split(', ')
-    control_types = dic_object['control_type'].split(', ')
-    auto_ids = dic_object['auto_id']
+    _setting("Setting 3", "Settings", dic_of_objects)
 
-    # Mảng chứa thông tin các object pass fail
-    pass_list = []
-    fail_list = []
+# Function test case setting 4
+def _setting_4():
+    # dictionaries
+    dic_of_objects = {
+        'title': 'Home, Recommended settings, Cloud storage, Bluetooth devices, Personalize your device, Try Microsoft 365',
+        'auto_id': ", TitleContent, , TitleContent, TitleContent, ",
+        'control_type': 'ListItem, Text, Text, Text, Text, Text',
+        'object_handle': 'click, view, view, view, view, view'
+    }
+    _setting("Setting 4", "Settings", dic_of_objects)
 
-    # Kiểm tra xem độ dài của các danh sách có khớp nhau không
-    if len(titles) == len(control_types):
-        for title, auto_id, control_type in zip(titles, auto_ids, control_types):
-            is_exist = object_is_exist(target_window, title, auto_id, control_type)
-            if is_exist:
-                pass_list.append(title)
-            else:
-                fail_list.append(title)
-    else:
-        print("Title và control type không đồng nhất")
-    # Đóng app
-    target_window.close()
-    write_log_setting('Setting 4', pass_list, fail_list)
+# Function test case setting 5
+def _setting_5():
+    # dictionaries
+    dic_of_objects = {
+        'title': 'System, Display, Brightness & color, Scale & layout, Related settings',
+        'auto_id': ", , , , ",
+        'control_type': 'ListItem, Text, Text, Text, Text',
+        'object_handle': 'click, click, view, view, view'
+    }
+    _setting("Setting 5", "Settings", dic_of_objects)
 
-# Hiển thị kết quả lên giao diện
+# Function test case setting 5
+def _setting_12():
+    # dictionaries
+    dic_of_objects = {
+        'title': 'System, Sound, Output, Input, Advanced',
+        'auto_id': ", , , , ",
+        'control_type': 'ListItem, Text, Text, Text, Text',
+        'object_handle': 'click, click, view, view, view'
+    }
+    _setting("Setting 12", "Settings", dic_of_objects)
+# Call function execute test case
 _setting_3()
 _setting_4()
+_setting_5()
+_setting_12()
+
+# Show result on UI
 for result in result_of_testcase:
     txt_result.append(f"{result}\n")
 window.show()
--- lib_common/common_lib.py
+++ lib_common/common_lib.py
@@ -3,32 +3,32 @@ from time import sleep
 from AppOpener import open
 from pywinauto import Application, Desktop
 
-def open_app(app_name):
+def _open_app(app_name):
     open(app_name, match_closest=False)
-    sleep(3)
+    sleep(5)
     app = Application(backend='uia').connect(title_re=app_name)
     target_window = app.window(title_re=app_name)
     return target_window
 
-def object_is_exist(window, title, auto_id, control_type):
-    object = window.child_window(title=title, auto_id=auto_id, control_type=control_type)
+def _object_is_exist(window, title, auto_id, control_type):
+    object_select = window.child_window(title=title, auto_id=auto_id, control_type=control_type)
     is_exist = False
-    if object.exists():
-        # print(f'{title} tồn tại --> Pass')
+    if object_select.exists():
+        print(title, 'viewed')
         sleep(2)
         is_exist = True
-    # else:
-    #     print(f'{title} không tồn tại --> Fail')
     return is_exist
 
-def object_click(window, title, auto_id, control_type):
-    object = window.child_window(title=title, auto_id=auto_id, control_type=control_type)
+def _object_click(window, title, auto_id, control_type):
+    object_select = window.child_window(title=title, auto_id=auto_id, control_type=control_type)
     is_exist = False
-    if object.exists():
-        object.click_input()
-        # print(f'{title} tồn tại --> Pass')
+    if object_select.exists():
+        object_select.click_input()
+        print(title, 'clicked')
         is_exist = True
         sleep(2)
-    # else:
-    #     print(f'{title} không tồn tại --> Fail')
     return is_exist
+
+# target_window = _open_app('Settings')
+# object = target_window.child_window(title='Accessibility', auto_id='', control_type='ListItem')
+# object.click_input()
